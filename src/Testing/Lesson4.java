678e751fc9786c482a55947b42a5cf7a1bb79bc5
DaveQa <daveqatester@gmail.com>
2023-04-30 20:06:34 -0400

    Adding new test lesson4

---
+++ src/Testing/Lesson4.java
@@ -0,0 +1,10 @@
+package Testing;
+
+public class Lesson4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Testout");
+	}
+
+}
--- src/fibonacci/fibonacciExample.java
+++ src/fibonacci/fibonacciExample.java
@@ -4,7 +4,7 @@ public class fibonacciExample {
 
 	class Main {
 		public static void main(String[] args) {
-
+//new one
 			int n = 12, firstTerm = 0, secondTerm = 1;
 			System.out.println("Fibonacci Series till " + n + " terms:");
 
